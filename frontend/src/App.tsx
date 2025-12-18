070c985b0be193b29e6e2b1de09c9672fe45eac8
wbulhoes <willian.bulhoes@actdigital.com>
2025-12-18 18:57:13 -0300

    [FRONTEND] fix: corrige erros TypeScript

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import { useState, useEffect } from 'react'
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import DadosEnergeticosLista from './components/DadosEnergeticosLista'
 import DadosEnergeticosForm from './components/DadosEnergeticosForm'
